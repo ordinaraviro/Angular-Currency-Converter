34224f388832e56d2c3933efbbe0755f7dfce856
Alexander_Yulin <ordinaraviro@gmail.com>
2023-02-24 17:41:22 +0200

    feat: add header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  public usdRate: number | undefined;
+  public eurRate: number | undefined;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getExchangeRates();
+  }
+
+  private getExchangeRates(): void {
+    const headers = new HttpHeaders({
+      'apikey': 'Tc7nsjUCyV66aVeWmW3QxBq6VIcryero'
+    });
+
+    this.http.get<any>('https://api.apilayer.com/exchangerates_data/latest?symbols=USD,EUR&base=UAH', { headers })
+      .subscribe(data => {
+        this.usdRate = data.rates.USD;
+        this.eurRate = data.rates.EUR;
+      });
+  }
+
+}
