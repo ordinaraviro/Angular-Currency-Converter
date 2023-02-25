67bce51c01a36c45402eaf12999c9b1bffa82717
Alexander_Yulin <ordinaraviro@gmail.com>
2023-02-25 16:30:18 +0200

    feat: done input funcs

--- src/app/components/converter/converter.component.ts
+++ src/app/components/converter/converter.component.ts
@@ -8,4 +8,34 @@ import { Component, Input } from '@angular/core';
 export class ConverterComponent {
   @Input() public usdRate: number | undefined;
   @Input() public eurRate: number | undefined;
+  input1 = 1;
+  input2 = 2;
+  rate1 = 'UAH';
+  rate2 = 'USD';
+
+  rateObj: Record<string, number> = {};
+
+  constructor() {}
+
+  ngOnInit() {
+    this.updateRateObj();
+  }
+
+  ngOnChanges() {
+    this.updateRateObj();
+  }
+
+  updateRateObj() {
+    this.rateObj = {
+      UAH: 1,
+      USD: this.usdRate || 0,
+      EUR: this.eurRate || 0
+    };
+  }
+
+  onInputChange(index = 1) {
+    index === 1 ?
+    this.input2 = +(this.input1 / this.rateObj[this.rate1] * this.rateObj[this.rate2]).toFixed(2):
+    this.input1 = +(this.input2 / this.rateObj[this.rate2] * this.rateObj[this.rate1]).toFixed(2);
+  }
 }
